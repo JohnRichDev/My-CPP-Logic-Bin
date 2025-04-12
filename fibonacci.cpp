03fef156ec71be0ea974cfa60f66c05ea73c9870
johnrichdev <johnrichpe@gmail.com>
2025-04-12 22:26:57 +0800

    finish fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
-int main() {
+int fib(int n)
+{
+    int result = 0;
+    if (n == 1 || n == 2)
+        return 1;
+
+    result = fib(n - 1) + fib(n - 2);
+
+    return result;
+}
+
+int main()
+{
+    std::cout << fib(10);
     return 0;
 }
